ffc4f7ca5e1ed9cb90ad67f23a0de0dbf5817c3f
Morad17 <morad123@hotmail.co.uk>
2023-01-03 23:22:23 +0000

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown'
 
 const Navbar = () => {
   return (
     <div className="navbar">
         <div className="wrapper">
-            <div className="left"></div>
+            <div className="left">
+                <div className="item">
+                    Logo
+                    
+                </div>
+            </div>
             <div className="center"></div>
             <div className="right"></div>
         </div>
